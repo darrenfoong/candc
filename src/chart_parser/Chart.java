darrenfoong/candc

commit d4efc2427ccec696723585aed2dca25f97b4a762

Make public static variables to be private, with getters and setters.

src/chart_parser/Chart.java
@@ -39,7 +39,15 @@ public class Chart {
 
 	public Weights weights;
 
-	public static int numSuperCategories;
+	private static int numSuperCategories;
+
+	public static void setNumSuperCategories(int n) {
+		numSuperCategories++;
+	}
+
+	public static int getNumSuperCategories() {
+		return numSuperCategories;
+	}
 
 	public Chart(int MAX_WORDS, boolean output, Relations relations, boolean oracleFscore, boolean trainingBeamParser) {
 		this.printDetailedOutput = output;
@@ -204,7 +212,7 @@ public class Chart {
 			equivFscore.clear();
 		}
 
-		SuperCategory.numSuperCategories = 0;
-		Chart.numSuperCategories = 0;
+		SuperCategory.setNumSuperCategories(0);
+		Chart.setNumSuperCategories(0);
 	}
 }

src/cat_combination/SuperCategory.java
@@ -90,13 +90,29 @@ public class SuperCategory implements Comparable<SuperCategory> {
 	 * of dependencies (correct or incorrect, but need to ignore those not taken
 	 * into account in the evaluation)
 	 */
-	static public IgnoreDepsEval ignoreDeps;
+	private static IgnoreDepsEval ignoreDeps;
+
+	public static void setIgnoreDepsEval(IgnoreDepsEval i) {
+		ignoreDeps = i;
+	}
+
+	public static IgnoreDepsEval getIgnoreDepsEval() {
+		return ignoreDeps;
+	}
 
 	/*
 	 * used as a global counter to count the number of categories being produced
 	 * for a sentence
 	 */
-	static public int numSuperCategories;
+	private static int numSuperCategories;
+
+	public static void setNumSuperCategories(int n) {
+		numSuperCategories = n;
+	}
+
+	public static int getNumSuperCategories() {
+		return numSuperCategories;
+	}
 
 	public SuperCategory(short headIndex, Category cat, short flags) {
 		this.cat = cat;

src/chart_parser/Cell.java
@@ -41,7 +41,7 @@ public class Cell {
 	public void decrementClearCounter() {
 		this.clearCounter--;
 		if ( clearCounter == 0 ) {
-			Chart.numSuperCategories -= superCategories.size();
+			Chart.setNumSuperCategories(Chart.getNumSuperCategories() - superCategories.size());
 			superCategories.clear();
 		}
 	}
@@ -72,7 +72,7 @@ public class Cell {
 				if (finalSize < listSize) {
 					superCategories.subList(finalSize, listSize).clear();
 					superCategories.trimToSize();
-					Chart.numSuperCategories -= (listSize - finalSize);
+					Chart.setNumSuperCategories(Chart.getNumSuperCategories() - (listSize - finalSize));
 				}
 			}
 		}
@@ -187,7 +187,7 @@ public class Cell {
 
 	public void add(SuperCategory superCat) {
 		superCategories.add(superCat);
-		Chart.numSuperCategories++;
+		Chart.setNumSuperCategories(Chart.getNumSuperCategories()+1);
 	}
 
 	public int size() {

src/chart_parser/ChartParser.java
@@ -134,7 +134,7 @@ public class ChartParser {
 			jloop:
 			for (int j = 2; j <= numWords; j++) {
 				for (int i = j - 2; i >= 0; i--) {
-					if (SuperCategory.numSuperCategories > MAX_SUPERCATS) {
+					if (SuperCategory.getNumSuperCategories() > MAX_SUPERCATS) {
 						maxSuperCatsExceeded = true;
 						System.out.println("MAX_SUPERCATS exceeded!");
 						log.println("MAX_SUPERCATS exceeded!");

src/chart_parser/ChartParserBeam.java
@@ -138,10 +138,10 @@ public class ChartParserBeam extends ChartParser {
 		jloop:
 		for (int j = 2; j <= numWords; j++) {
 			for (int i = j - 2; i >= 0; i--) {
-				if (Chart.numSuperCategories > MAX_SUPERCATS) {
+				if (Chart.getNumSuperCategories() > MAX_SUPERCATS) {
 					maxSuperCatsExceeded = true;
-					System.out.println("MAX_SUPERCATS exceeded. (" + Chart.numSuperCategories + " > " + MAX_SUPERCATS + ")");
-					log.println("MAX_SUPERCATS exceeded. (" + Chart.numSuperCategories + " > " + MAX_SUPERCATS + ")");
+					System.out.println("MAX_SUPERCATS exceeded. (" + Chart.getNumSuperCategories() + " > " + MAX_SUPERCATS + ")");
+					log.println("MAX_SUPERCATS exceeded. (" + Chart.getNumSuperCategories() + " > " + MAX_SUPERCATS + ")");
 					break jloop;
 				}
 

src/chart_parser/OracleFscoreDecoder.java
@@ -26,7 +26,7 @@ public class OracleFscoreDecoder extends OracleDecoder {
 		 * static variable, since it's only used in conjunction with this
 		 * decoder:
 		 */
-		SuperCategory.ignoreDeps = ignoreDeps;
+		SuperCategory.setIgnoreDepsEval(ignoreDeps);
 	}
 
 	@Override

src/model/Features.java
@@ -127,7 +127,15 @@ public class Features {
 
 	public int numFeatures;
 
-	public static boolean newFeatures;
+	private static boolean newFeatures;
+
+	public static void setNewFeatures(boolean n) {
+		newFeatures = n;
+	}
+
+	public static boolean getNewFeatures() {
+		return newFeatures;
+	}
 
 	public Features(String featuresFile, Categories categories, boolean newFeatures) throws IOException {
 		this.featureCatIDs = new FeatureIDs<FeatureCat>();
@@ -141,7 +149,7 @@ public class Features {
 
 		readFeatures(featuresFile, categories);
 
-		Features.newFeatures = newFeatures;
+		Features.setNewFeatures(newFeatures);
 	}
 
 	public Features(String featuresFile, String weightsFile, Weights weights, Categories categories, boolean newFeatures) throws IOException {
@@ -156,7 +164,7 @@ public class Features {
 
 		readFeaturesWeights(featuresFile, weightsFile, weights, categories);
 
-		Features.newFeatures = newFeatures;
+		Features.setNewFeatures(newFeatures);
 	}
 
 	private void readFeatures(String featuresFile, Categories categories) throws IOException {

commit 1435f4607959de5d1883c84f819cd5a8949f4749

Fix bug in setNumSuperCategories() (introduced in d4efc24).

src/chart_parser/Chart.java
@@ -42,7 +42,7 @@ public class Chart {
 	private static int numSuperCategories;
 
 	public static void setNumSuperCategories(int n) {
-		numSuperCategories++;
+		numSuperCategories = n;
 	}
 
 	public static int getNumSuperCategories() {