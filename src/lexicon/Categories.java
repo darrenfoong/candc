3a6bd528bc8192f68456e7a148e3c55fe7067101
Darren Foong <darrenfoong@gmail.com>
2016-01-29 21:43:16 +0000

    Refactor Categories

--- src/lexicon/Categories.java
+++ src/lexicon/Categories.java
@@ -38,8 +38,7 @@ public class Categories {
 	public ArrayList<TypeRaisedCategory> trAPs;
 	public ArrayList<TypeRaisedCategory> trVP_TOs;
 
-	// ALT_MARKEDUP signals to use the alternative lines in the
-	// markedup file (marked ! in the file)
+	// ALT_MARKEDUP signals to use the alternative lines in the markedup file (marked ! in the file)
 	public Categories(String grammarDir, boolean ALT_MARKEDUP) {
 		dependencyRelations = new Relations();
 		readMarkedupFile(grammarDir, ALT_MARKEDUP);
@@ -81,14 +80,16 @@ public class Categories {
 		plainCategoryStrings = new HashMap<String, String>();
 		markedupCategories = new HashMap<String, Category>();
 
-		// not sure when the sentinel is used for categories, but
-		// retaining from C&C:
+		// not sure when the sentinel is used for categories, but retaining from C&C:
 		markedupStrings.put("+", "+");
 		markedupCategories.put("+", null);
 
 		String markedupFile = grammarDir + "/markedup";
+
+		BufferedReader in = null;
+
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(markedupFile));
+			in = new BufferedReader(new FileReader(markedupFile));
 			Preface.readPreface(in);
 
 			States state = States.CAT;
@@ -96,102 +97,106 @@ public class Categories {
 			String plainCatString = "";
 			String markedupCatString = "";
 			String line = "";
-			while ((line = in.readLine()) != null) {
-				if (line.length() == 0) {
-					if (state != States.GRS) {
+
+			while ( (line = in.readLine()) != null ) {
+				if ( line.length() == 0 ) {
+					if ( state != States.GRS ) {
 						throw new Error("missing markedup or GRS");
 					}
 					state = States.CAT;
 					continue;
 				}
-				if (line.charAt(0) == '#') {
+
+				if ( line.charAt(0) == '#' ) {
 					continue;
 				}
-				if (line.charAt(0) == '=') {
+
+				if ( line.charAt(0) == '=' ) {
 					String[] tokens = line.split("\\s");
-					if (tokens.length < 2) {
-						throw new Error(
-								"error parsing constraints line in markedup");
+					if ( tokens.length < 2 ) {
+						throw new Error("error parsing constraints line in markedup");
 					}
 					continue;
 				}
+
 				Category cat;
-				switch (state) {
-				case CAT:
-					String[] tokens = (line.trim()).split("\\s+");
-					if (tokens.length != 1) {
-						throw new Error(
-								"error parsing plain cat line in markedup");
-					}
-					plainCatString = tokens[0];
-					state = States.MARKEDUP;
-					break;
-				case MARKEDUP:
-					tokens = (line.trim()).split("\\s+");
+				switch ( state ) {
+					case CAT:
+						String[] tokens = (line.trim()).split("\\s+");
+						if ( tokens.length != 1 ) {
+							throw new Error("error parsing plain cat line in markedup");
+						}
+						plainCatString = tokens[0];
+						state = States.MARKEDUP;
+						break;
+					case MARKEDUP:
+						tokens = (line.trim()).split("\\s+");
 
-					if (tokens.length != 2) {
-						throw new Error(
-								"error parsing markedup cat line in markedup");
-					}
-
-					markedupCatString = tokens[1];
-					cat = parse(markedupCatString);
-
-					/*
-					 * call reorder since C&C does, although this Java class
-					 * does now check the variable ordering in the consumeSlot
-					 * method
-					 * 
-					 * ensures categories have vars ordered according to the
-					 * ordering in the VarID class:
-					 */
-					byte[] seenVariables = new byte[VarID.NUM_VARS];
-					Arrays.fill(seenVariables, VarID.NONE); // not necessary
-					// since VarID.NONE
-					// is 0, but let's
-					// be explicit
-
-					cat.reorderVariables(seenVariables, new ByteWrapper(
-							(byte) (0)));
-
-					markedupStrings.put(plainCatString, markedupCatString);
-					plainCategoryStrings.put(markedupCatString, plainCatString);
-					markedupCategories.put(plainCatString, cat);
-
-					state = States.GRS;
-					break;
-				case GRS:
-					tokens = line.split("\\s");
-					if (tokens.length < 2) {
-						throw new Error("error parsing gr cat line in markedup");
-					}
-					String tmp = tokens[0];
-					if (tmp == "!") {
-						if (!ALT_MARKEDUP) {
-							continue;
+						if ( tokens.length != 2 ) {
+							throw new Error("error parsing markedup cat line in markedup");
 						}
 
 						markedupCatString = tokens[1];
 						cat = parse(markedupCatString);
 
-						seenVariables = new byte[VarID.NUM_VARS];
-						Arrays.fill(seenVariables, VarID.NONE);
+						/*
+						 * call reorder since C&C does, although this Java class
+						 * does now check the variable ordering in the
+						 * consumeSlot method
+						 * 
+						 * ensures categories have vars ordered according to the
+						 * ordering in the VarID class:
+						 */
+						byte[] seenVariables = new byte[VarID.NUM_VARS];
+						Arrays.fill(seenVariables, VarID.NONE); // not necessary
+						// since VarID.NONE
+						// is 0, but let's
+						// be explicit
 
-						cat.reorderVariables(seenVariables, new ByteWrapper(
-								(byte) (0)));
+						cat.reorderVariables(seenVariables, new ByteWrapper((byte) (0)));
 
 						markedupStrings.put(plainCatString, markedupCatString);
-						plainCategoryStrings.put(markedupCatString,
-								plainCatString);
+						plainCategoryStrings.put(markedupCatString, plainCatString);
 						markedupCategories.put(plainCatString, cat);
 
-						continue;
-					}
-					break;
+						state = States.GRS;
+						break;
+					case GRS:
+						tokens = line.split("\\s");
+						if ( tokens.length < 2 ) {
+							throw new Error("error parsing gr cat line in markedup");
+						}
+						String tmp = tokens[0];
+						if ( tmp == "!" ) {
+							if ( !ALT_MARKEDUP ) {
+								continue;
+							}
+
+							markedupCatString = tokens[1];
+							cat = parse(markedupCatString);
+
+							seenVariables = new byte[VarID.NUM_VARS];
+							Arrays.fill(seenVariables, VarID.NONE);
+
+							cat.reorderVariables(seenVariables, new ByteWrapper((byte) (0)));
+
+							markedupStrings.put(plainCatString, markedupCatString);
+							plainCategoryStrings.put(markedupCatString, plainCatString);
+							markedupCategories.put(plainCatString, cat);
+
+							continue;
+						}
+						break;
 				}
 			}
-		} catch (IOException e) {
+		} catch ( IOException e ) {
 			System.err.println(e);
+		} finally {
+			try {
+				if ( in != null ) { in.close(); }
+			} catch ( IOException e ) {
+				System.err.println(e);
+			}
 		}
 	}
 
@@ -205,49 +210,41 @@ public class Categories {
 		ShortWrapper prevSlot = new ShortWrapper((short) (0));
 		ShortWrapper index = new ShortWrapper((short) (0));
 
-		Category cat = consumeCategory(catString, index, true, numJuliaSlots,
-				prevSlot);
+		Category cat = consumeCategory(catString, index, true, numJuliaSlots, prevSlot);
 
-		if (index.value != catString.length()) {
+		if ( index.value != catString.length() ) {
 			throw new Error("failed to parse category string: " + catString);
 		}
 
 		return cat;
 	}
 
-	private Category consumeCategory(String markedupString, ShortWrapper index,
-			boolean inResult, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
-		switch (markedupString.charAt(index.value)) {
-		case '(':
-			index.value++;
-			return consumeComplex(markedupString, index, inResult,
-					numJuliaSlots, prevSlot);
-		case ')': // fall through
-		case '\\': // |
-		case '/': // |
-			throw new Error("failed to parse category string" + markedupString);
-		default:
-			return consumeBasic(markedupString, index, numJuliaSlots, prevSlot);
+	private Category consumeCategory(String markedupString, ShortWrapper index, boolean inResult, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
+		switch ( markedupString.charAt(index.value) ) {
+			case '(':
+				index.value++;
+				return consumeComplex(markedupString, index, inResult, numJuliaSlots, prevSlot);
+			case ')': // fall through
+			case '\\': // |
+			case '/': // |
+				throw new Error("failed to parse category string" + markedupString);
+			default:
+				return consumeBasic(markedupString, index, numJuliaSlots, prevSlot);
 		}
 	}
 
-	private Category consumeBasic(String markedupString, ShortWrapper index,
-			ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
+	private Category consumeBasic(String markedupString, ShortWrapper index, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
 		char currentChar = markedupString.charAt(index.value);
 
-		if (!Character.isLetter(currentChar) && currentChar != ','
-				&& currentChar != '.' && currentChar != ';'
-				&& currentChar != ':') {
+		if ( !Character.isLetter(currentChar) && currentChar != ',' && currentChar != '.' && currentChar != ';' && currentChar != ':' ) {
 			throw new Error("unexpected token parsing category in consumeBasic");
 		}
 
 		StringBuilder atomStringBuilder = new StringBuilder();
-		while (Character.isLetter(currentChar) || currentChar == ','
-				|| currentChar == '.' || currentChar == ';'
-				|| currentChar == ':') {
+		while ( Character.isLetter(currentChar) || currentChar == ',' || currentChar == '.' || currentChar == ';' || currentChar == ':' ) {
 			atomStringBuilder.append(currentChar);
 			index.value++;
-			if (index.value < markedupString.length()) {
+			if ( index.value < markedupString.length() ) {
 				currentChar = markedupString.charAt(index.value);
 			} else {
 				break;
@@ -255,11 +252,9 @@ public class Categories {
 		}
 		String atomString = atomStringBuilder.toString();
 		Atom atom = new Atom(atomString);
-		GrammaticalFeature feature = new GrammaticalFeature(
-				GrammaticalFeature.NONE);
+		GrammaticalFeature feature = new GrammaticalFeature(GrammaticalFeature.NONE);
 
-		if (index.value < markedupString.length()
-				&& markedupString.charAt(index.value) == '[') {
+		if ( index.value < markedupString.length() && markedupString.charAt(index.value) == '[' ) {
 			index.value++;
 			feature = consumeFeature(markedupString, index);
 		}
@@ -267,53 +262,48 @@ public class Categories {
 		byte var = VarID.NONE;
 		ShortWrapper lrange = new ShortWrapper((short) (0));
 
-		if (index.value < markedupString.length()
-				&& markedupString.charAt(index.value) == '{') {
+		if ( index.value < markedupString.length() && markedupString.charAt(index.value) == '{' ) {
 			index.value++;
 			var = consumeVar(markedupString, index, lrange);
 		}
+
 		short slot = 0;
 
-		if (index.value < markedupString.length()
-				&& markedupString.charAt(index.value) == '<') {
+		if ( index.value < markedupString.length() && markedupString.charAt(index.value) == '<' ) {
 			index.value++;
 			slot = consumeSlot(markedupString, index, numJuliaSlots, prevSlot);
 		}
+
 		checkBracketChar(markedupString, index);
 
 		return new Category(atom, feature, var, slot, lrange.value);
 	}
 
-	private Category consumeComplex(String markedupString, ShortWrapper index,
-			boolean inResult, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
-		Category left = consumeCategory(markedupString, index, inResult,
-				numJuliaSlots, prevSlot);
+	private Category consumeComplex(String markedupString, ShortWrapper index, boolean inResult, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
+		Category left = consumeCategory(markedupString, index, inResult, numJuliaSlots, prevSlot);
 
 		char currentChar = markedupString.charAt(index.value);
 		byte slash = 0;
 
-		if (currentChar == '/') {
+		if ( currentChar == '/' ) {
 			slash = Category.FWD_SLASH;
-		} else if (currentChar == '\\') {
+		} else if ( currentChar == '\\' ) {
 			slash = Category.BWD_SLASH;
 		} else {
-			throw new Error("expected a forward or backward slash"
-					+ markedupString);
+			throw new Error("expected a forward or backward slash" + markedupString);
 		}
 
 		index.value++;
 
-		if (inResult) {
+		if ( inResult ) {
 			numJuliaSlots.value++;
 		}
 
-		Category right = consumeCategory(markedupString, index, false,
-				numJuliaSlots, prevSlot);
+		Category right = consumeCategory(markedupString, index, false, numJuliaSlots, prevSlot);
 
 		currentChar = markedupString.charAt(index.value);
-		if (currentChar != ')') {
-			throw new Error("unbalanced parentheses in category string"
-					+ markedupString);
+		if ( currentChar != ')' ) {
+			throw new Error("unbalanced parentheses in category string" + markedupString);
 		}
 
 		index.value++;
@@ -321,15 +311,13 @@ public class Categories {
 		byte var = VarID.NONE;
 		ShortWrapper lrange = new ShortWrapper((short) (0));
 
-		if (index.value < markedupString.length()
-				&& markedupString.charAt(index.value) == '{') {
+		if ( index.value < markedupString.length() && markedupString.charAt(index.value) == '{' ) {
 			index.value++;
 			var = consumeVar(markedupString, index, lrange);
 		}
 		short slot = 0;
 
-		if (index.value < markedupString.length()
-				&& markedupString.charAt(index.value) == '<') {
+		if ( index.value < markedupString.length() && markedupString.charAt(index.value) == '<' ) {
 			index.value++;
 			slot = consumeSlot(markedupString, index, numJuliaSlots, prevSlot);
 		}
@@ -338,17 +326,16 @@ public class Categories {
 		return new Category(left, slash, right, var, slot, lrange.value);
 	}
 
-	private GrammaticalFeature consumeFeature(String markedupString,
-			ShortWrapper index) {
+	private GrammaticalFeature consumeFeature(String markedupString, ShortWrapper index) {
 		char currentChar = markedupString.charAt(index.value);
 
 		StringBuilder featureStringBuilder = new StringBuilder();
-		while (Character.isLetter(currentChar)) {
+		while ( Character.isLetter(currentChar) ) {
 			featureStringBuilder.append(currentChar);
 			currentChar = markedupString.charAt(++index.value);
 		}
 		String featureString = featureStringBuilder.toString();
-		if (currentChar != ']') {
+		if ( currentChar != ']' ) {
 			throw new Error("unexpected character parsing feature");
 		}
 
@@ -357,17 +344,16 @@ public class Categories {
 		return new GrammaticalFeature(featureString);
 	}
 
-	private byte consumeVar(String markedupString, ShortWrapper index,
-			ShortWrapper lrange) {
+	private byte consumeVar(String markedupString, ShortWrapper index, ShortWrapper lrange) {
 		char currentChar = markedupString.charAt(index.value);
 
 		StringBuilder varStringBuilder = new StringBuilder();
-		while (Character.isLetter(currentChar) || currentChar == '_') {
+		while ( Character.isLetter(currentChar) || currentChar == '_' ) {
 			varStringBuilder.append(currentChar);
 			currentChar = markedupString.charAt(++index.value);
 		}
 		String varString = varStringBuilder.toString();
-		if (currentChar == '*') {
+		if ( currentChar == '*' ) {
 			/*
 			 * from the C&C code: lrange.value = markedup.size(); this assumes
 			 * that the markedup data structure is ordered and categories can be
@@ -378,9 +364,8 @@ public class Categories {
 			lrange.value = 1;
 			currentChar = markedupString.charAt(++index.value);
 		}
-		if (currentChar != '}') {
-			throw new Error("unexpected character parsing var: "
-					+ markedupString + " " + currentChar);
+		if ( currentChar != '}' ) {
+			throw new Error("unexpected character parsing var: " + markedupString + " " + currentChar);
 		}
 
 		index.value++;
@@ -388,17 +373,16 @@ public class Categories {
 		return VarID.convert(varString);
 	}
 
-	private short consumeSlot(String markedupString, ShortWrapper index,
-			ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
+	private short consumeSlot(String markedupString, ShortWrapper index, ShortWrapper numJuliaSlots, ShortWrapper prevSlot) {
 		char currentChar = markedupString.charAt(index.value);
 
 		StringBuilder slotStringBuilder = new StringBuilder();
-		while (Character.isDigit(currentChar)) {
+		while ( Character.isDigit(currentChar) ) {
 			slotStringBuilder.append(currentChar);
 			currentChar = markedupString.charAt(++index.value);
 		}
 		String slotString = slotStringBuilder.toString();
-		if (currentChar != '>') {
+		if ( currentChar != '>' ) {
 			throw new Error("unexpected character parsing dependency");
 		}
 
@@ -410,23 +394,20 @@ public class Categories {
 		 * the slots are ordered from 1, increasing by 1 each time:
 		 */
 		short slotShort = Short.parseShort(slotString);
-		if (slotShort != prevSlot.value + 1) {
-			throw new Error("slots are not increasing by 1! " + markedupString
-					+ " " + slotShort + " " + prevSlot.value);
+		if ( slotShort != prevSlot.value + 1 ) {
+			throw new Error("slots are not increasing by 1! " + markedupString + " " + slotShort + " " + prevSlot.value);
 		}
 		prevSlot.value++;
 
-		return dependencyRelations.addRelation(markedupString, slotShort,
-				numJuliaSlots.value);
+		return dependencyRelations.addRelation(markedupString, slotShort, numJuliaSlots.value);
 	}
 
 	private void checkBracketChar(String markedupString, ShortWrapper index) {
-		if (index.value < markedupString.length()) {
+		if ( index.value < markedupString.length() ) {
 			char currentChar = markedupString.charAt(index.value);
 
-			if (currentChar == '<' || currentChar == '(' || currentChar == '!') {
-				throw new Error(
-						"unexpected nesting or dependency parsing basic category");
+			if ( currentChar == '<' || currentChar == '(' || currentChar == '!' ) {
+				throw new Error("unexpected nesting or dependency parsing basic category");
 			}
 		}
 	}
@@ -438,10 +419,11 @@ public class Categories {
 	 * indicates what the variable is associated with the dependency, which
 	 * needs carrying over
 	 */
-	private void readTRcategories(String trFile,
-			ArrayList<TypeRaisedCategory> trList) {
+	private void readTRcategories(String trFile, ArrayList<TypeRaisedCategory> trList) {
+		BufferedReader in = null;
+
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(trFile));
+			in = new BufferedReader(new FileReader(trFile));
 
 			String markedupCatString = "";
 			String varString = "";
@@ -450,11 +432,10 @@ public class Categories {
 			Preface.readPreface(in);
 
 			String line = "";
-			while ((line = in.readLine()) != null) {
+			while ( (line = in.readLine()) != null ) {
 				String[] tokens = line.split("\\s");
-				if (tokens.length != 2) {
-					throw new Error(
-							"expecting 2 items per line in the TR file!");
+				if ( tokens.length != 2 ) {
+					throw new Error("expecting 2 items per line in the TR file!");
 				}
 
 				markedupCatString = tokens[0];
@@ -476,20 +457,25 @@ public class Categories {
 				 * Variable; seenVariables[depVar] is the new variable for the
 				 * dependency variable (see TypeRaisedCategory.java)
 				 */
-				TypeRaisedCategory trCat = new TypeRaisedCategory(cat,
-						seenVariables[VarID.X], seenVariables[depVar]);
+				TypeRaisedCategory trCat = new TypeRaisedCategory(cat, seenVariables[VarID.X], seenVariables[depVar]);
 				trList.add(trCat);
 			}
-		} catch (IOException e) {
+		} catch ( IOException e ) {
 			System.err.println(e);
+		} finally {
+			try {
+				if ( in != null ) { in.close(); }
+			} catch ( IOException e ) {
+				System.err.println(e);
+			}
 		}
 	}
 
 	public Category canonize(String catString) {
 		Category cat = seenCategories.get(catString);
-		if (cat == null) {
+		if ( cat == null ) {
 			cat = parse(catString);
-			if (cat == null) {
+			if ( cat == null ) {
 				throw new Error("failed to parse catString!" + catString);
 			}
 			canonicalCats.put(cat, cat);
@@ -503,7 +489,7 @@ public class Categories {
 	 */
 	public Category canonize(Category checkCat) {
 		Category cat = canonicalCats.get(checkCat);
-		if (cat == null) {
+		if ( cat == null ) {
 			canonicalCats.put(checkCat, checkCat);
 			return checkCat;
 		} else {
