9284495a3dde79367fa3e1832ae1993ffe80f834
Darren Foong <darrenfoong@gmail.com>
2016-02-02 20:53:18 +0000

    Refactor TrainParserBeam

--- src/TrainParserBeam.java
+++ src/TrainParserBeam.java
@@ -18,11 +18,11 @@ import chart_parser.OracleDepsSumDecoder;
 
 public class TrainParserBeam {
 	public static void main(String[] args) {
-		int MAX_WORDS = 150;
-		int MAX_SUPERCATS = 500000;
+		int MAX_WORDS = 250;
+		int MAX_SUPERCATS = 50000;
 
 		boolean altMarkedup = false;
-		boolean eisnerNormalForm = true;
+		boolean eisnerNormalForm = false;
 		boolean detailedOutput = false;
 		boolean newFeatures = false;
 		boolean cubePruning = false;
@@ -66,7 +66,7 @@ public class TrainParserBeam {
 
 		try {
 			lexicon = new Lexicon(lexiconFile);
-		} catch (IOException e) {
+		} catch ( IOException e ) {
 			System.err.println(e);
 			return;
 		}
@@ -77,9 +77,8 @@ public class TrainParserBeam {
 			parser = new ChartTrainParserBeam(grammarDir, altMarkedup,
 					eisnerNormalForm, MAX_WORDS, MAX_SUPERCATS, detailedOutput,
 					ruleInstancesParams, lexicon, featuresFile, weightsFile,
-					newFeatures, cubePruning, beamSize, beta, parallelUpdate,
-					updateLogP);
-		} catch (IOException e) {
+					newFeatures, cubePruning, beamSize, beta, parallelUpdate, updateLogP);
+		} catch ( IOException e ) {
 			System.err.println(e);
 			return;
 		}
@@ -89,17 +88,17 @@ public class TrainParserBeam {
 		int numTrainInstances = 1;
 
 		try ( PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputWeightsFile)));
-			  PrintWriter log = new PrintWriter(new BufferedWriter(new FileWriter(logFile)));
-			  PrintWriter writer = new PrintWriter(System.out) ) {
+				PrintWriter log = new PrintWriter(new BufferedWriter(new FileWriter(logFile)));
+				PrintWriter writer = new PrintWriter(System.out) ) {
 
 			Preface.printPreface(out);
 
-			for (int iteration = 1; iteration <= numIterations; iteration++) {
+			for ( int iteration = 1; iteration <= numIterations; iteration++ ) {
 				try ( BufferedReader in = new BufferedReader(new FileReader(inputFile));
-					  BufferedReader goldDeps = new BufferedReader(new FileReader(goldDepsFile));
-					  BufferedReader goldDepsPerCell = new BufferedReader(new FileReader(goldDepsFile + ".per_cell"));
-					  BufferedReader roots = new BufferedReader(new FileReader(rootCatsFile));
-					  PrintWriter outIter = new PrintWriter(new BufferedWriter(new FileWriter(outputWeightsFile + "." + iteration))) ) {
+						BufferedReader goldDeps = new BufferedReader(new FileReader(goldDepsFile));
+						BufferedReader goldDepsPerCell = new BufferedReader(new FileReader(goldDepsFile + ".per_cell"));
+						BufferedReader roots = new BufferedReader(new FileReader(rootCatsFile));
+						PrintWriter outIter = new PrintWriter(new BufferedWriter(new FileWriter(outputWeightsFile + "." + iteration))) ) {
 
 					Preface.readPreface(in);
 					Preface.readPreface(goldDeps);
@@ -112,20 +111,19 @@ public class TrainParserBeam {
 					Sentences sentences = new Sentences(in, null, parser.categories, MAX_WORDS);
 					sentences.skip(fromSentence - 1);
 
-					for (int numSentence = fromSentence; numSentence <= toSentence && sentences.hasNext(); numSentence++) {
-						System.out.println("Parsing sentence " + iteration + "/"+ numSentence);
-						log.println("Parsing sentence " + iteration + "/"+ numSentence);
+					for ( int numSentence = fromSentence; numSentence <= toSentence && sentences.hasNext(); numSentence++ ) {
+						System.out.println("Parsing sentence " + iteration + "/" + numSentence);
+						log.println("Parsing sentence " + iteration + "/" + numSentence);
 
 						parser.parseSentence(sentences.next(), log, betas);
 
 						oracleDecoder.readDeps(goldDeps, parser.categories);
-						// oracleDecoder.readRootCat(roots, parser.categories);
 
 						if ( oracleDecoder.numGoldDeps() != 0 ) {
 							numTrainInstances++;
 							SuperCategory best = parser.updateWeights(log, numTrainInstances);
 
-							if (best != null) {
+							if ( best != null ) {
 								System.out.println("best category deps: ");
 								parser.printDeps(writer, parser.categories.dependencyRelations, parser.sentence, best);
 								writer.flush();
@@ -137,13 +135,13 @@ public class TrainParserBeam {
 								log.println();
 							}
 						} else {
-							System.out.println("No gold dependencies for sentence " + iteration + "/"+ numSentence);
+							System.out.println("No gold dependencies for sentence " + iteration + "/" + numSentence);
 							System.out.println();
-							log.println("No gold dependencies for sentence " + iteration + "/"+ numSentence);
+							log.println("No gold dependencies for sentence " + iteration + "/" + numSentence);
 							log.println();
 						}
 
-						if (numSentence % 5000 == 0) {
+						if ( numSentence % 5000 == 0 ) {
 							try ( PrintWriter outIterPart = new PrintWriter(new BufferedWriter(new FileWriter(outputWeightsFile + "." + iteration + "." + numSentence))) ) {
 								Preface.printPreface(outIterPart);
 								parser.printWeights(outIterPart, numTrainInstances);
@@ -151,7 +149,7 @@ public class TrainParserBeam {
 						}
 					}
 
-					System.out.println("# Statistics for iteration " + iteration + ": hasUpdate: " + parser.hasUpdateStatistics.calcHasUpdates() + "/" + parser.hasUpdateStatistics.getSize() + " (" + ((double) parser.hasUpdateStatistics.calcHasUpdates()/(double) parser.hasUpdateStatistics.getSize()) + ")");
+					System.out.println("# Statistics for iteration " + iteration + ": hasUpdate: " + parser.hasUpdateStatistics.calcHasUpdates() + "/" + parser.hasUpdateStatistics.getSize() + " (" + ((double) parser.hasUpdateStatistics.calcHasUpdates() / (double) parser.hasUpdateStatistics.getSize()) + ")");
 					System.out.println("# Statistics for iteration " + iteration + ": hypothesisSize: " + parser.hypothesisSizeStatistics.calcAverageProportion() + " (" + parser.hypothesisSizeStatistics.getSize() + ")");
 
 					Preface.printPreface(outIter);
@@ -160,9 +158,9 @@ public class TrainParserBeam {
 			}
 
 			parser.printWeights(out, numTrainInstances);
-		} catch (FileNotFoundException e) {
+		} catch ( FileNotFoundException e ) {
 			System.err.println(e);
-		} catch (IOException e) {
+		} catch ( IOException e ) {
 			System.err.println(e);
 		}
 	}
